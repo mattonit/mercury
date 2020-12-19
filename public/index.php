e2dd88f07e85f5f95adbdc6701dbbcfd12e5b88c
mattonit <malan95@gmail.com>
2020-12-20 00:07:17 +0100

    added autoloader

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Registering the autoloader
+ *
+ * Composer provides automatically-generated class loader for the
+ * application so there's no need to manually include each of them
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
